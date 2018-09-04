f346315550fc53380527f9e7fb064a5c5434c548
Orlando-Silva <orlando.silva.contato@gmail.com>
2018-09-04 20:31:55 -0300

    Refatoração no POCO.

--- RedeSocial/Core/Entidades/Comentario.cs
+++ RedeSocial/Core/Entidades/Comentario.cs
@@ -7,13 +7,9 @@ namespace Core.Entidades
     public class Comentario
     {
         public int ID { get; set; }
-
         public string Conteudo { get; set; }
-
         public DateTime Criado { get; set; }
-
         public Postagem Postagem { get; set; }
-
         public Usuario Autor { get; set; }
     }
 }
--- RedeSocial/Core/Entidades/FotoDePerfil.cs
+++ RedeSocial/Core/Entidades/FotoDePerfil.cs
@@ -7,17 +7,12 @@ namespace Core.Entidades
     public class FotoDePerfil
     {
         public int ID { get; set; }
-
         public string Nome { get; set; }
-
         public string Extensao { get; set; }
-
-        public byte[] Tamanho { get; set; }
-
+        public decimal Tamanho { get; set; }
         public string Caminho { get; set; }
-
         public Status Status { get; set; }
-
         public string Hash { get; set; }
+        public Usuario Usuario { get; set; }
     }
 }
