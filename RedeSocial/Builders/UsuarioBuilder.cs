00ac60b6622111090f91b33483181eeb462fd813
Aluno <Aluno@DOSO121756>
2018-09-11 08:39:59 -0300

    Trabalhando no Builder genérico e no Builder do Usuário.

--- RedeSocial/Builders/UsuarioBuilder.cs
+++ RedeSocial/Builders/UsuarioBuilder.cs
@@ -8,102 +8,77 @@ using System.Linq;
 
 namespace Builders
 {
-    public class UsuarioBuilder
+    public class UsuarioBuilder : Builder<Usuario>
     {
-        #region --Atributos--
-        private static Usuario Usuario { get; set; }
-        #endregion
-
-        #region --Construtores--
-        /// <summary>
-        /// Inicializa um construtor de usuário para um novo usuário.
-        /// </summary>
-        public UsuarioBuilder()
-        {
-            Usuario = new Usuario();
-        }
-        /// <summary>
-        /// Inicializa um construtor de usuário para um usuário existente.
-        /// </summary>
-        public UsuarioBuilder(Usuario _usuario)
-        {
-            Usuario = _usuario ?? throw new ArgumentNullException("Usuário nulo.");
-        }
-        #endregion
-
         #region --Construtores de Usuário--
         public UsuarioBuilder ComNome(string nome)
         {
-            Usuario.Nome = nome ?? throw new ArgumentNullException("Nome nulo.");
+            Entidade.Nome = nome ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(nome) } não pode ser nulo.", paramName: nameof(nome));
             return this;
         }
 
         public UsuarioBuilder ComSenha(string senha)
         {
             if (senha is null)
-                throw new ArgumentNullException("Senha nula.");
-            
-            Usuario.Senha = Seguranca.ComputeHash(senha, Seguranca.GenerateSalt()).ToString();
+                throw new ArgumentNullException(message: $"O parâmetro { nameof(senha) } não pode ser nulo.", paramName: nameof(senha));
+
+            Entidade.Senha = Seguranca.ComputeHash(senha, Seguranca.GenerateSalt()).ToString();
             return this;
         }
 
         public UsuarioBuilder ComLogin(string login)
         {
-            Usuario.Login = login ?? throw new ArgumentNullException("Login nulo.");
+            Entidade.Login = login ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(login) } não pode ser nulo.", paramName: nameof(login));
             return this;
         }
 
         public UsuarioBuilder NaRua(string rua)
         {
-            Usuario.Rua = rua ?? throw new ArgumentNullException("Rua nula.");
+            Entidade.Rua = rua ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(rua) } não pode ser nulo.", paramName: nameof(rua));
             return this;
         }
 
         public UsuarioBuilder NaCidade(string cidade)
         {
-            Usuario.Cidade = cidade ?? throw new ArgumentNullException("Cidade nula.");
+            Entidade.Cidade = cidade ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(cidade) } não pode ser nulo.", paramName: nameof(cidade));
             return this;
         }
 
         public UsuarioBuilder NoEstado(string estado)
         {
-            Usuario.Estado = estado ?? throw new ArgumentNullException("Estado nulo.");
-            return this;
+            Entidade.Estado = estado ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(estado) } não pode ser nulo.", paramName: nameof(estado));
+            return this; 
         }
 
         public UsuarioBuilder NoPais(string pais)
         {
-            Usuario.Pais = pais ?? throw new ArgumentNullException("País nulo.");
+            Entidade.Pais = pais ?? throw new ArgumentNullException(message: $"O parâmetro { nameof(pais) } não pode ser nulo.", paramName: nameof(pais));
             return this;
         }
 
         public UsuarioBuilder ComAmigos(IList<Usuario> amigos)
         {
-            Usuario.Amigos = amigos.Any() ? amigos : throw new ArgumentNullException("Lista de amigos nula.");
+            Entidade.Amigos = amigos.Any() ? amigos : throw new ArgumentNullException(message: $"O parâmetro { nameof(amigos) } não pode ser nulo.", paramName: nameof(amigos));
             return this;
         }
 
         public UsuarioBuilder ComFotos(IList<FotoDePerfil> fotos)
         {
-            Usuario.Fotos = fotos.Any() ? fotos : throw new ArgumentNullException("Lista de fotos nula.");
+            Entidade.Fotos = fotos.Any() ? fotos : throw new ArgumentNullException(message: $"O parâmetro { nameof(fotos) } não pode ser nulo.", paramName: nameof(fotos));
             return this;
         }
 
         public UsuarioBuilder ComPostagens(IList<Postagem> postagems)
         {
-            Usuario.Postagens = postagems.Any() ? postagems : throw new ArgumentNullException("Lista de postagens nula.");
+            Entidade.Postagens = postagems.Any() ? postagems : throw new ArgumentNullException(message: $"O parâmetro { nameof(postagems) } não pode ser nulo.", paramName: nameof(postagems));
             return this;
         }
 
         public UsuarioBuilder ComComentarios(IList<Comentario> comentarios)
         {
-            Usuario.Comentarios = comentarios.Any() ? comentarios : throw new ArgumentNullException("Lista de comentários nula.");
+            Entidade.Comentarios = comentarios.Any() ? comentarios : throw new ArgumentNullException(message: $"O parâmetro { nameof(comentarios) } não pode ser nulo.", paramName: nameof(comentarios));
             return this;
         }
-        #endregion
-
-        #region --Operador de Conversão Implicita--
-        public static implicit operator Usuario(UsuarioBuilder _usuarioBuilder) => Usuario;
         #endregion
     }
 }
